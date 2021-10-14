c587744e09c42659d8cf901e3fc57d4a6dbdf551
rukkiecodes <rukkiecodes@gmail.com>
2021-10-14 19:46:33 +0100

    added style manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,119 @@
 import React, { useEffect } from 'react'
-import Grapesjs from 'grapesjs';
+import grapesjs from 'grapesjs';
 import 'grapesjs/dist/css/grapes.min.css';
 import GrapesConfig from "./GrapesConfig"
-import './App.css'
+import './app.css'
 
-function App () {
-  useEffect(() => { loadGrapesJs() })
+
+const App = () => {
+  useEffect(() => { loadGrapesJs() }, [])
+  const loadComponents = (editor) => {
+    editor.BlockManager.add('my-block-id', {
+      label: "<b>Block</b>",
+      content: {
+        tagName: 'div',
+        draggable: true,
+        attributes: { 'some-attribute': 'some-value' },
+        components: [
+          {
+            tagName: 'span',
+            content: '<b>Some static content</b>',
+          }, {
+            tagName: 'div',
+            // use `content` for static strings, `components` string will be parsed
+            // and transformed in Components
+            components: '<span>HTML at some point</span>',
+          }
+        ]
+      }
+    })
+    editor.Panels.addPanel({
+      id: 'panel-top',
+      el: '.panel__top',
+    });
+    editor.Panels.addPanel({
+      id: 'basic-actions',
+      el: '.panel__basic-actions',
+      buttons: [
+        {
+          id: 'visibility',
+          active: true, // active by default
+          className: 'btn-toggle-borders',
+          label: '<u>B</u>',
+          command: 'sw-visibility', // Built-in command
+        }, {
+          id: 'export',
+          className: 'btn-open-export',
+          label: 'Exp',
+          command: 'export-template',
+          context: 'export-template', // For grouping context of buttons from the same panel
+        }, {
+          id: 'show-json',
+          className: 'btn-show-json',
+          label: 'JSON',
+          context: 'show-json',
+          command (editor) {
+            editor.Modal.setTitle('Components JSON')
+              .setContent(`<textarea style="width:100%; height: 250px;">
+            ${JSON.stringify(editor.getComponents())}
+          </textarea>`)
+              .open();
+          },
+        }
+      ],
+    });
+    // Define commands
+    // editor.Commands.add('show-layers', {
+    //   getRowEl (editor) { return editor.getContainer().closest('.editor-row'); },
+    //   getLayersEl (row) { return row.querySelector('.layers-container') },
+
+    //   run (editor, sender) {
+    //     const lmEl = this.getLayersEl(this.getRowEl(editor));
+    //     lmEl.style.display = '';
+    //   },
+    //   stop (editor, sender) {
+    //     const lmEl = this.getLayersEl(this.getRowEl(editor));
+    //     lmEl.style.display = 'none';
+    //   },
+    // });
+
+    // editor.Commands.add('show-styles', {
+    //   getRowEl (editor) { return editor.getContainer().closest('.editor-row'); },
+    //   getStyleEl (row) { return row.querySelector('.styles-container') },
+
+    //   run (editor, sender) {
+    //     const smEl = this.getStyleEl(this.getRowEl(editor));
+    //     smEl.style.display = '';
+    //   },
+    //   stop (editor, sender) {
+    //     const smEl = this.getStyleEl(this.getRowEl(editor));
+    //     smEl.style.display = 'none';
+    //   },
+    // });
+  }
   const loadGrapesJs = async () => {
-    const editor = await Grapesjs.init(GrapesConfig())
+    const editor = await grapesjs.init(GrapesConfig())
+    loadComponents(editor)
   }
   return (
-    <div className="App">
-      <div id="gjs">
-        <h1>Hello world component</h1>
+    <>
+      <div className="panel__top">
+        <div className="panel__basic-actions"></div>
+        <div className="panel__switcher"></div>
       </div>
-    </div>
+      <div className="editor-row">
+        <div className="editor-canvas">
+          <div id="gjs">
+            <h1>Hello world component</h1>
+          </div>
+        </div>
+        <div className="panel__right">
+          <div className="layers-container"></div>
+          <div className="styles-container"></div>
+        </div>
+      </div>
+      <div id="blocks"></div>
+    </>
   )
 }
-
 export default App
--- src/GrapesConfig.jsx
+++ src/GrapesConfig.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import 'grapesjs/dist/css/grapes.min.css';
-import './App.css'
 
 const GrapesConfig = () => {
   return {
@@ -12,10 +11,90 @@ const GrapesConfig = () => {
     // Size of the editor
     height: '300px',
     width: 'auto',
+
+    // append blocks
+    blockManager: {
+      appendTo: '#blocks',
+    },
+
+    layerManager: {
+      appendTo: '.layers-container'
+    },
+    // We define a default panel as a sidebar to contain layers
+    panels: {
+      defaults: [
+        // ...
+        {
+          id: 'panel-switcher',
+          el: '.panel__switcher',
+          buttons: [{
+            id: 'show-layers',
+            active: true,
+            label: 'Layers',
+            command: 'show-layers',
+            // Once activated disable the possibility to turn it off
+            togglable: false,
+          }, {
+            id: 'show-style',
+            active: true,
+            label: 'Styles',
+            command: 'show-styles',
+            togglable: false,
+          }],
+        }
+      ]
+    },
+    // The Selector Manager allows to assign classes and
+    // different states (eg. :hover) on components.
+    // Generally, it's used in conjunction with Style Manager
+    // but it's not mandatory
+    selectorManager: {
+      appendTo: '.styles-container'
+    },
+    styleManager: {
+      appendTo: '.styles-container',
+      sectors: [{
+        name: 'Dimension',
+        open: false,
+        // Use built-in properties
+        buildProps: ['width', 'min-height', 'padding', 'display', 'color', 'background-color'],
+        // Use `properties` to define/override single property
+        properties: [
+          {
+            // Type of the input,
+            // options: integer | radio | select | color | slider | file | composite | stack
+            type: 'integer',
+            name: 'The width', // Label for the property
+            property: 'width', // CSS property (if buildProps contains it will be extended)
+            units: ['px', '%', 'pt', 'em', 'rem', 'vh', 'vw', 'mm', 'cm'], // Units, available only for 'integer' types
+            defaults: 'auto', // Default value
+            min: 0, // Min value, available only for 'integer' types
+          }
+        ]
+      }, {
+        name: 'Extra',
+        open: false,
+        buildProps: ['background-color', 'box-shadow', 'custom-prop', 'transform'],
+        properties: [
+          {
+            id: 'custom-prop',
+            name: 'Custom Label',
+            property: 'font-size',
+            type: 'select',
+            defaults: '32px',
+            // List of options, available only for 'select' and 'radio'  types
+            options: [
+              { value: '12px', name: 'Tiny' },
+              { value: '18px', name: 'Medium' },
+              { value: '32px', name: 'Big' },
+            ],
+          }
+        ]
+      }]
+    },
+
     // Disable the storage manager for the moment
     storageManager: false,
-    // Avoid any default panel
-    panels: { defaults: [] },
   }
 }
 
